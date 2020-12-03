16d7c52804d5d71c3c46aff347bb330777ba9055
Tahmid Zahin <tahmidzahin@Jeffs-MacBook-Pro.local>
2020-12-03 18:03:27 +0100

    Task 1 implemented now it is possible to play the game

--- agents/common.py
+++ agents/common.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from typing import Optional
+from typing import Callable, Tuple
 import numpy as np
 
 BoardPiece = np.int8  # The data type (dtype) of the board
@@ -8,6 +9,16 @@ PLAYER1 = BoardPiece(1)  # board[i, j] == PLAYER1 where player 1 has a piece
 PLAYER2 = BoardPiece(2)  # board[i, j] == PLAYER2 where player 2 has a piece
 
 PlayerAction = np.int8  # The column to be played
+
+
+class SavedState:
+    pass
+
+
+GenMove = Callable[
+    [np.ndarray, BoardPiece, Optional[SavedState]],  # Arguments for the generate_move function
+    Tuple[PlayerAction, Optional[SavedState]]  # Return type of the generate_move function
+]
 
 
 class GameState(Enum):
@@ -69,13 +80,14 @@ def string_to_board(pp_board: str) -> np.ndarray:
         row = row.replace('|', '')
         for colindex in range(0, len(row)):
             if colindex == 0 or (colindex % 2) == 0:
-                boardCol = int(colindex/2)
+                boardCol = int(colindex / 2)
                 if row[colindex] == "X":
                     board[index, boardCol] = PLAYER1
                 elif row[colindex] == "O":
                     board[index, boardCol] = PLAYER2
 
     return board
+
 
 def apply_player_action(
         board: np.ndarray, action: PlayerAction, player: BoardPiece, copy: bool = False
@@ -84,8 +96,18 @@ def apply_player_action(
     Sets board[i, action] = player, where i is the lowest open row. The modified
     board is returned. If copy is True, makes a copy of the board before modifying it.
     """
-    raise NotImplementedError()
-
+    for index, item in enumerate(board[:, action]):
+        if item != NO_PLAYER and index != 0:
+            if copy:
+                boardCopy = board.copy()
+                boardCopy[index - 1, action] = player
+                return boardCopy
+            else:
+                board[index - 1, action] = player
+                return board
+        if item == NO_PLAYER and index == board.shape[0]-1:
+            board[index, action] = player
+            return board
 
 def connected_four(
         board: np.ndarray, player: BoardPiece, last_action: Optional[PlayerAction] = None,
@@ -96,7 +118,22 @@ def connected_four(
     If desired, the last action taken (i.e. last column played) can be provided
     for potential speed optimisation.
     """
-    raise NotImplementedError()
+    diags = get_diags_of_matrix(board)
+    winner = find_winner_from_list(diags)
+
+    if winner != NO_PLAYER and winner != player:
+        return False
+    if winner != NO_PLAYER and winner == player:
+        return True
+    else:
+        winner = find_winner_from_list(board.copy())
+        if winner != NO_PLAYER and winner == player:
+            return True
+        winner = find_winner_from_list(board.copy().T)
+        if winner != NO_PLAYER and winner == player:
+            return True
+
+    return False
 
 
 def check_end_state(
@@ -107,4 +144,87 @@ def check_end_state(
     action won (GameState.IS_WIN) or drawn (GameState.IS_DRAW) the game,
     or is play still on-going (GameState.STILL_PLAYING)?
     """
-    raise NotImplementedError()
+    result = connected_four(board, player)
+    if result:
+        return GameState['IS_WIN']
+
+    copyBoard = board.copy()
+    if BoardPiece == PLAYER1:
+        copyBoard = copyBoard.reshape(6 * 7)
+        for index, item in enumerate(copyBoard):
+            if item == NO_PLAYER:
+                copyBoard[index] = PLAYER2
+        copyBoard.reshape(6, 7)
+        result = connected_four(copyBoard, PLAYER2)
+        if result == False:
+            return GameState['IS_DRAW']
+
+    if BoardPiece == PLAYER2:
+        copyBoard = copyBoard.reshape(6 * 7)
+        for index, item in enumerate(copyBoard):
+            if item == NO_PLAYER:
+                copyBoard[index] = PLAYER1
+        copyBoard.reshape(6, 7)
+        result = connected_four(copyBoard, PLAYER1)
+        if result == False:
+            return GameState['IS_DRAW']
+
+    return GameState['STILL_PLAYING']
+
+
+def get_diags_of_matrix(board: np.ndarray) -> list:
+    a = board.copy()
+    # a.diagonal returns the top-left-to-lower-right diagonal "i"
+    # according to this diagram:
+    #
+    #  0  1  2  3  4 ...
+    # -1  0  1  2  3
+    # -2 -1  0  1  2
+    # -3 -2 -1  0  1
+    #  :
+    #
+    # You wanted lower-left-to-upper-right and upper-left-to-lower-right diagonals.
+    #
+    # The syntax a[slice,slice] returns a new array with elements from the sliced ranges,
+    # where "slice" is Python's [start[:stop[:step]] format.
+
+    # "::-1" returns the rows in reverse. ":" returns the columns as is,
+    # effectively vertically mirroring the original array so the wanted diagonals are
+    # lower-right-to-uppper-left.
+    #
+    # Then a list comprehension is used to collect all the diagonals.  The range
+    # is -x+1 to y (exclusive of y), so for a matrix like the example above
+    # (x,y) = (4,5) = -3 to 4.
+    diags = [a[::-1, :].diagonal(i) for i in range(-a.shape[0] + 1, a.shape[1])]
+
+    # Now back to the original array to get the upper-left-to-lower-right diagonals,
+    # starting from the right, so the range needed for shape (x,y) was y-1 to -x+1 descending.
+    diags.extend(a.diagonal(i) for i in range(a.shape[1] - 1, -a.shape[0], -1))
+
+    # Another list comp to convert back to Python lists from numpy arrays,
+    # so it prints what you requested.
+    result = list(filter(lambda item: len(item) >= 4, [n.tolist() for n in diags]))
+    return result
+
+
+def find_winner_from_list(list):
+    winner = NO_PLAYER
+    for data in list:
+        matchData = [NO_PLAYER, NO_PLAYER]
+        matchCount = 0
+        for index, player in enumerate(data):
+            matchData[0] = player
+            if matchData[1] != matchData[0]:
+                matchData[1] = player
+                matchCount = 1
+            elif matchData[1] == matchData[0] and matchData[1] != NO_PLAYER:
+                matchCount += 1
+                if matchCount == 4:
+                    winner = player
+                    break
+        if winner == NO_PLAYER:
+            continue
+        else:
+            break
+
+    return winner
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,9 +1,11 @@
 import numpy as np
 import numpy.testing as nptest
 import sys, os
+from agents.common import BoardPiece, NO_PLAYER, PLAYER1, PLAYER2, PlayerAction, initialize_game_state, \
+    pretty_print_board, string_to_board, apply_player_action, connected_four
+
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
-from agents.common import BoardPiece, NO_PLAYER, PLAYER1, PLAYER2
 
 desiredBoardString = "|==============|\n|              |\n|              |\n|    X X       |\n|    O X X     |\n|  O " \
                      "X O " \
@@ -28,7 +30,6 @@ desiredBoardNp[5, 4] = PLAYER1
 class TestBoard:
 
     def test_board_init(self):
-        from agents.common import initialize_game_state
         actual = initialize_game_state()
         assert isinstance(actual, np.ndarray)
         assert actual.dtype == BoardPiece
@@ -36,12 +37,38 @@ class TestBoard:
         assert np.all(actual == NO_PLAYER)
 
     def test_pretty_board_print(self, capsys):
-        from agents.common import pretty_print_board
         ret = pretty_print_board(desiredBoardNp)
-        captured = capsys.readouterr()
         assert ret == desiredBoardString
 
     def test_string_to_board(self):
-        from agents.common import string_to_board
         board = string_to_board(desiredBoardString)
         nptest.assert_array_equal(board, desiredBoardNp)
+
+    def test_apply_player_action(self):
+        copyBoard = desiredBoardNp.copy()
+        copyBoard[3, 1] = PLAYER1
+        modifiedBoard = apply_player_action(desiredBoardNp, PlayerAction(1), PLAYER1)
+        assert np.array_equal(desiredBoardNp, copyBoard)
+        assert np.array_equal(desiredBoardNp, modifiedBoard)
+        modifiedBoard = apply_player_action(desiredBoardNp, PlayerAction(1), PLAYER1, True)
+        assert np.array_equal(desiredBoardNp, modifiedBoard) == False
+
+    def test_connected_four(self):
+        # Check column
+        copyBoardForColTest = desiredBoardNp.copy()
+        copyBoardForColTest[4, 3] = PLAYER1
+        copyBoardForColTest[1, 3] = PLAYER1
+        assert connected_four(copyBoardForColTest, PLAYER1) == True
+        assert connected_four(copyBoardForColTest, PLAYER2) == False
+        # Check diagonal
+        copyBoardForDiagTest = desiredBoardNp.copy()
+        copyBoardForDiagTest[5, 1] = PLAYER1
+        copyBoardForDiagTest[2, 4] = PLAYER1
+        assert connected_four(copyBoardForColTest, PLAYER1) == True
+        assert connected_four(copyBoardForColTest, PLAYER2) == False
+        # Check row
+        copyBoardForRowTest = desiredBoardNp.copy()
+        copyBoardForRowTest[5, 1] = PLAYER1
+        copyBoardForRowTest[5, 2] = PLAYER1
+        assert connected_four(copyBoardForRowTest, PLAYER1) == True
+        assert connected_four(copyBoardForRowTest, PLAYER2) == False
